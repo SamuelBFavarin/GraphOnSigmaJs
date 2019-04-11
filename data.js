6d5eb2482c28ac0cc3d2257876f892e5e930cb87
SamuelBFavarin <samuelbfavarin@hotmail.com>
2019-04-11 09:31:20 -0300

    base data

---
+++ data.js
@@ -0,0 +1,664 @@
+var data = [{
+	"table": "TabelaDesconhecida",
+	"rows":
+	[
+		{
+			"id_valuer": 2991,
+			"id_employee": 2997,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 40
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 3000,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Leonardo Caetano",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 3001,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Beth Navas",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 3003,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 3006,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 11561,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Armando Macena de Lima Junior",
+			"total_weight": 150
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 11822,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 19769,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Leandro Grison",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 2991,
+			"id_employee": 39669,
+			"valuer_name": "Renato Navas",
+			"employee_name": "Daliane Felin",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 2997,
+			"id_employee": 2991,
+			"valuer_name": "Fabio Bucior",
+			"employee_name": "Renato Navas",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 2997,
+			"id_employee": 3000,
+			"valuer_name": "Fabio Bucior",
+			"employee_name": "Leonardo Caetano",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 2997,
+			"id_employee": 3003,
+			"valuer_name": "Fabio Bucior",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 2997,
+			"id_employee": 11822,
+			"valuer_name": "Fabio Bucior",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 40
+		},
+		{
+			"id_valuer": 2997,
+			"id_employee": 14198,
+			"valuer_name": "Fabio Bucior",
+			"employee_name": "Rafael Marini",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 2997,
+			"id_employee": 19769,
+			"valuer_name": "Fabio Bucior",
+			"employee_name": "Leandro Grison",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 2997,
+			"id_employee": 38976,
+			"valuer_name": "Fabio Bucior",
+			"employee_name": "Marco Aur�lio Sim�o",
+			"total_weight": 150
+		},
+		{
+			"id_valuer": 3000,
+			"id_employee": 2991,
+			"valuer_name": "Leonardo Caetano",
+			"employee_name": "Renato Navas",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3000,
+			"id_employee": 3002,
+			"valuer_name": "Leonardo Caetano",
+			"employee_name": "Michelly Dellecave",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3000,
+			"id_employee": 3003,
+			"valuer_name": "Leonardo Caetano",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 3000,
+			"id_employee": 3006,
+			"valuer_name": "Leonardo Caetano",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3000,
+			"id_employee": 11561,
+			"valuer_name": "Leonardo Caetano",
+			"employee_name": "Armando Macena de Lima Junior",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3000,
+			"id_employee": 11822,
+			"valuer_name": "Leonardo Caetano",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 2991,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Renato Navas",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 2997,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 3003,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 70
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 3006,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 9213,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Willian Alves",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 11561,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Armando Macena de Lima Junior",
+			"total_weight": 150
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 11822,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 120
+		},
+		{
+			"id_valuer": 3002,
+			"id_employee": 38976,
+			"valuer_name": "Michelly Dellecave",
+			"employee_name": "Marco Aur�lio Sim�o",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 2997,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 3002,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Michelly Dellecave",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 3006,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 11560,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Samuel Favarin",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 11822,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 40
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 14198,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Rafael Marini",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 19769,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Leandro Grison",
+			"total_weight": 70
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 38976,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Marco Aur�lio Sim�o",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3003,
+			"id_employee": 39268,
+			"valuer_name": "Cesar Nanci",
+			"employee_name": "Andressa Juliana",
+			"total_weight": 120
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 2991,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Renato Navas",
+			"total_weight": 150
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 2997,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 3000,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Leonardo Caetano",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 3002,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Michelly Dellecave",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 3003,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 9213,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Willian Alves",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 11561,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Armando Macena de Lima Junior",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 11822,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 70
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 31703,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Bruno Alves",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 3006,
+			"id_employee": 39669,
+			"valuer_name": "Tiago Barboza",
+			"employee_name": "Daliane Felin",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 2991,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Renato Navas",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 2997,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 40
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 3002,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Michelly Dellecave",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 3006,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 150
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 11560,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Samuel Favarin",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 11822,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 14198,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Rafael Marini",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 38976,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Marco Aur�lio Sim�o",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 39268,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Andressa Juliana",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 9213,
+			"id_employee": 39669,
+			"valuer_name": "Willian Alves",
+			"employee_name": "Daliane Felin",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 11560,
+			"id_employee": 2997,
+			"valuer_name": "Samuel Favarin",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 150
+		},
+		{
+			"id_valuer": 11560,
+			"id_employee": 3000,
+			"valuer_name": "Samuel Favarin",
+			"employee_name": "Leonardo Caetano",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 11560,
+			"id_employee": 3003,
+			"valuer_name": "Samuel Favarin",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 40
+		},
+		{
+			"id_valuer": 11560,
+			"id_employee": 11822,
+			"valuer_name": "Samuel Favarin",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 11560,
+			"id_employee": 14198,
+			"valuer_name": "Samuel Favarin",
+			"employee_name": "Rafael Marini",
+			"total_weight": 120
+		},
+		{
+			"id_valuer": 11560,
+			"id_employee": 19769,
+			"valuer_name": "Samuel Favarin",
+			"employee_name": "Leandro Grison",
+			"total_weight": 170
+		},
+		{
+			"id_valuer": 11561,
+			"id_employee": 3002,
+			"valuer_name": "Armando Macena de Lima Junior",
+			"employee_name": "Michelly Dellecave",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 11561,
+			"id_employee": 3006,
+			"valuer_name": "Armando Macena de Lima Junior",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 11561,
+			"id_employee": 11822,
+			"valuer_name": "Armando Macena de Lima Junior",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 2991,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Renato Navas",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 2997,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 3000,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Leonardo Caetano",
+			"total_weight": 70
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 3001,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Beth Navas",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 3002,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Michelly Dellecave",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 3006,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 70
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 11561,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Armando Macena de Lima Junior",
+			"total_weight": 70
+		},
+		{
+			"id_valuer": 11822,
+			"id_employee": 39669,
+			"valuer_name": "Laryssa Ayala Nunes",
+			"employee_name": "Daliane Felin",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 14198,
+			"id_employee": 2997,
+			"valuer_name": "Rafael Marini",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 14198,
+			"id_employee": 3003,
+			"valuer_name": "Rafael Marini",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 14198,
+			"id_employee": 11560,
+			"valuer_name": "Rafael Marini",
+			"employee_name": "Samuel Favarin",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 14198,
+			"id_employee": 11822,
+			"valuer_name": "Rafael Marini",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 14198,
+			"id_employee": 19769,
+			"valuer_name": "Rafael Marini",
+			"employee_name": "Leandro Grison",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 19769,
+			"id_employee": 2991,
+			"valuer_name": "Leandro Grison",
+			"employee_name": "Renato Navas",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 19769,
+			"id_employee": 2997,
+			"valuer_name": "Leandro Grison",
+			"employee_name": "Fabio Bucior",
+			"total_weight": 170
+		},
+		{
+			"id_valuer": 19769,
+			"id_employee": 3003,
+			"valuer_name": "Leandro Grison",
+			"employee_name": "Cesar Nanci",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 19769,
+			"id_employee": 11560,
+			"valuer_name": "Leandro Grison",
+			"employee_name": "Samuel Favarin",
+			"total_weight": 150
+		},
+		{
+			"id_valuer": 19769,
+			"id_employee": 14198,
+			"valuer_name": "Leandro Grison",
+			"employee_name": "Rafael Marini",
+			"total_weight": 40
+		},
+		{
+			"id_valuer": 31703,
+			"id_employee": 3000,
+			"valuer_name": "Bruno Alves",
+			"employee_name": "Leonardo Caetano",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 31703,
+			"id_employee": 3001,
+			"valuer_name": "Bruno Alves",
+			"employee_name": "Beth Navas",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 31703,
+			"id_employee": 3002,
+			"valuer_name": "Bruno Alves",
+			"employee_name": "Michelly Dellecave",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 31703,
+			"id_employee": 3006,
+			"valuer_name": "Bruno Alves",
+			"employee_name": "Tiago Barboza",
+			"total_weight": 20
+		},
+		{
+			"id_valuer": 31703,
+			"id_employee": 11561,
+			"valuer_name": "Bruno Alves",
+			"employee_name": "Armando Macena de Lima Junior",
+			"total_weight": 100
+		},
+		{
+			"id_valuer": 31703,
+			"id_employee": 11822,
+			"valuer_name": "Bruno Alves",
+			"employee_name": "Laryssa Ayala Nunes",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 39942,
+			"id_employee": 3000,
+			"valuer_name": "Bianca Santos",
+			"employee_name": "Leonardo Caetano",
+			"total_weight": 50
+		},
+		{
+			"id_valuer": 39942,
+			"id_employee": 39268,
+			"valuer_name": "Bianca Santos",
+			"employee_name": "Andressa Juliana",
+			"total_weight": 50
+		}
+	]
+}]
---
+++ users.js
@@ -0,0 +1,86 @@
+var users = [{
+	"table": "colaboradores",
+	"rows":
+	[
+		{
+			"id_colaborador": 2991,
+			"nome": "Renato"
+		},
+		{
+			"id_colaborador": 2997,
+			"nome": "Fabio"
+		},
+		{
+			"id_colaborador": 3000,
+			"nome": "Leonardo"
+		},
+		{
+			"id_colaborador": 3001,
+			"nome": "Beth"
+		},
+		{
+			"id_colaborador": 3002,
+			"nome": "Michelly"
+		},
+		{
+			"id_colaborador": 3003,
+			"nome": "Cesar"
+		},
+		{
+			"id_colaborador": 3006,
+			"nome": "Tiago"
+		},
+		{
+			"id_colaborador": 9213,
+			"nome": "Willian"
+		},
+		{
+			"id_colaborador": 11560,
+			"nome": "Samuel"
+		},
+		{
+			"id_colaborador": 11561,
+			"nome": "Armando"
+		},
+		{
+			"id_colaborador": 11822,
+			"nome": "Laryssa"
+		},
+		{
+			"id_colaborador": 14198,
+			"nome": "Rafael"
+		},
+		{
+			"id_colaborador": 19769,
+			"nome": "Leandro"
+		},
+		{
+			"id_colaborador": 31703,
+			"nome": "Bruno"
+		},
+		{
+			"id_colaborador": 37745,
+			"nome": "Comite"
+		},
+		{
+			"id_colaborador": 38976,
+			"nome": "Marco"
+		},
+		{
+			"id_colaborador": 39268,
+			"nome": "Andressa"
+		},
+		{
+			"id_colaborador": 39669,
+			"nome": "Daliane"
+		},
+		{
+			"id_colaborador": 39942,
+			"nome": "Bianca"
+		},
+		{
+			"id_colaborador": 40434,
+			"nome": "Diego"
+		}
+	]
+}]
